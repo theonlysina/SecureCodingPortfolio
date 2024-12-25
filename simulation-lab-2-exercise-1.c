fb9b8d72d6c491728319d3b0af1b07815870c675
Sina Nazari <37281266+theonlysina@users.noreply.github.com>
2024-12-25 14:14:23 -0500

    Update simulation-lab-2-exercise-1.c

--- simulation-lab-2-exercise-1.c
+++ simulation-lab-2-exercise-1.c
@@ -9,23 +9,24 @@ void encodeShellString(char *dest, size_t size, const char *src) {
     snprintf(dest, size, "\"%s\"", src); // Simple quoting for demonstration
 }
 
-// This program prints the size of a specified file in bytes
+// main function of the progra; prints size of a file in bytes
 int main(int argc, char** argv) {
-    // Ensure that the user supplied exactly one command line argument
+    // makes sure the user supplied exactly one command line argument
     if (argc != 2) { 
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-
-    {
-        // the user string is encoded by a library routine.
-        char filePath[BUFSIZE] = {0};
-        encodeShellString(filePath, sizeof(filePath), argv[1]);
 	
+	// the user string is encoded by a library routine.
+	char filePath[BUFSIZE] = {0};
+	encodeShellString(filePath, sizeof(filePath), argv[1]);
+		
+	// safely construct command (a string)
 	char cmd[BUFSIZE] = {0};
 	snprintf(cmd, sizeof(cmd), "wc -c < \"%s\"", filePath);
+	
+	# execute command
 	system(cmd);
-   }
 
     return 0;
 }
